646223af95823e206936e372ff34ffab9aad5ba2
jimey104 <jimey104@naver.com>
2025-05-26 18:07:59 +0900

    change dns server

--- flight-reservation-gateway/src/main/java/com/example/demo/flightreservationgateway/config/CorsConfig.java
+++ flight-reservation-gateway/src/main/java/com/example/demo/flightreservationgateway/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
     @Bean
     public CorsWebFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOriginPatterns(List.of("http://localhost:5173"));
+        config.setAllowedOriginPatterns(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
